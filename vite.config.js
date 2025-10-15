cf7476b43b00d1e9313035d9676af97ff850b759
RAJAT <mondalrajat231@gmail.com>
2025-10-15 16:48:14 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// import process from "process";
+import process from "process";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import cesium from "vite-plugin-cesium";
-
+import cesium from "vite-plugin-cesium"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
     tailwindcss(),
     cesium()
   ],
-  // base:"/PS-227",
+  base:process.env.VITE_BASE_PATH ||"/PS-227",
 
 })
 
